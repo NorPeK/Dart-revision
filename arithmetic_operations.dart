e15a91eb0a7163f50b7db3d19766c3f495ddf5fd
NorPeK <norpekbusiness@hotmail.com>
2024-07-24 18:55:58 +0300

    arithmetic operations

---
+++ arithmetic_operations.dart
@@ -0,0 +1,14 @@
+void main() {
+  int a = 4;
+  int b = 6;
+  int c = a + b;
+  int d = a - b;
+  double e = a / b;
+  int f = a * b;
+  int g = a % b; //4
+  print(c);
+  print(d);
+  print(e);
+  print(f);
+  print(g);
+}
