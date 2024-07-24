5324c75ac31e1ef1a8857f9de9d354f035db64c3
NorPeK <norpekbusiness@hotmail.com>
2024-07-24 18:50:25 +0300

    null safety

---
+++ null_safety.dart
@@ -0,0 +1,7 @@
+void main() {
+  //String name ; This gives error not null.
+  String? name; //meaning this string will output run if not assigned not error.
+  late String name2; //This late means that the string will take a value later.
+  name2 = "NorPeK";
+  print(name2); // output null
+}
