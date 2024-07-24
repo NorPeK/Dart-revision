fb459dc0c5fbc8f5bb51e985fa44d011179842f5
NorPeK <norpekbusiness@hotmail.com>
2024-07-24 19:53:03 +0300

    quality and relation operators

---
+++ quality_and_relation_operators.dart
@@ -0,0 +1,10 @@
+void main() {
+  int a = 10;
+  int b = 20;
+  int c = 10;
+  print(a == b); // false
+  print(a == c); // true
+  print(a != c); //false
+  print(a < b); //true
+  print(a >= c); // true
+}
