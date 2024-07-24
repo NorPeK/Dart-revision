2f88354645fc0e9ba626b9cbd11c32b0dbe29c73
NorPeK <norpekbusiness@hotmail.com>
2024-07-24 18:37:05 +0300

    datatypes

---
+++ datatype.dart
@@ -0,0 +1,8 @@
+void main() {
+  String firstname = "NorPeK";
+  String lastname = "Farron";
+  int age = 12;
+  double w = 2.3; // if w = 2 then output is 2.0
+  bool r = true;
+  print("$firstname $lastname"); // to print in a single line.
+}
