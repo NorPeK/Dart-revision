fe2af68be25ab5bfccc977f01c6731d8aa6fde51
NorPeK <norpekbusiness@hotmail.com>
2024-07-28 16:55:28 +0300

    string methods

---
+++ string_methods.dart
@@ -0,0 +1,8 @@
+void main() {
+  String firstname = "Nor";
+  String lastname = "PeK";
+  String name = "";
+  print(name.isEmpty); //true
+  print(firstname.length);
+  print(lastname.toUpperCase());
+}
