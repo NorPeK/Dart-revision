3bb07f2d64a7d4f2b8e838ebc0e2315a3a93965c
NorPeK <norpekbusiness@hotmail.com>
2024-07-28 15:10:12 +0300

    switch case

---
+++ switch.dart
@@ -0,0 +1,21 @@
+void main() {
+  int price = 1000;
+
+  switch (price) {
+    case 1000:
+      {
+        print("I have 1000");
+      }
+      break;
+    case 2000:
+      {
+        print("I have 2000");
+      }
+      break;
+    default:
+      {
+        print("I don't have money");
+      }
+      break;
+  }
+}
