c9a6cfd4645273e2a7aa7cfbce13087f0e0221f4
NorPeK <norpekbusiness@hotmail.com>
2024-07-30 16:05:25 +0300

    function types

--- function.dart
+++ function.dart
@@ -6,9 +6,19 @@ void main() {
   printName(); // execute the function.
 
   sumNumber(int a, int b) {
+    // this is a void function
     int c = a + b;
     print(c);
   }
 
   sumNumber(10, 20);
+
+  int sumNumber2(int a, int b) {
+    // this is a void function
+    int c = a + b;
+    print(c);
+    return (c);
+  }
+
+  int r = sumNumber2(10, 15);
 }
