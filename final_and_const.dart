ba22086775245ed6956e6c96731ec9e2567f8cdc
NorPeK <norpekbusiness@hotmail.com>
2024-07-30 15:35:54 +0300

    final and const

---
+++ final_and_const.dart
@@ -0,0 +1,7 @@
+void main() {
+  const String name =
+      "NorPeK"; // final do the same job except we can send a variable to it only once.
+
+  //name = "norpek"; this will give an error
+  print(name);
+}
