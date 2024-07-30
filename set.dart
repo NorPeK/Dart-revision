0d80ed2f939d7a74fda6ef052afa818768fd2dec
NorPeK <norpekbusiness@hotmail.com>
2024-07-30 15:42:23 +0300

    sets

---
+++ set.dart
@@ -0,0 +1,10 @@
+void main() {
+  Set names = {
+    "NorPeK",
+    "Rami",
+    "Ousama",
+    "NorPeK"
+  }; //this is not a map, meaning it can't have a key,value. No duplicates.
+
+  print(names); //NorPeK will be printed once.
+}
