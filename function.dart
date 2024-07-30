f67a44bb392b1b153b6299cfea97846ab0d2478f
NorPeK <norpekbusiness@hotmail.com>
2024-07-30 15:58:44 +0300

    function

---
+++ function.dart
@@ -0,0 +1,14 @@
+void main() {
+  printName() {
+    print("NorPeK");
+  }
+
+  printName(); // execute the function.
+
+  sumNumber(int a, int b) {
+    int c = a + b;
+    print(c);
+  }
+
+  sumNumber(10, 20);
+}
