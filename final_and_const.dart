51cd3415ad2d3dcbe54091ecdeeb970895aa1f5d
NorPeK <norpekbusiness@hotmail.com>
2024-08-04 17:48:46 +0300

    super

--- final_and_const.dart
+++ final_and_const.dart
@@ -1,6 +1,6 @@
 void main() {
   const String name =
-      "NorPeK"; // final do the same job except we can send a variable to it only once.
+      "NorPeK"; // final do the same job except we can send a variable to it only once. meaning that the value can only be known during runtime
 
   //name = "norpek"; this will give an error
   print(name);
---
+++ super.dart
@@ -0,0 +1,20 @@
+void main() {
+  Two two = new Two();
+  print(two.getVar);
+}
+
+class One {
+  final username;
+  One(this.username);
+  String one = "One";
+}
+
+class Two extends One {
+  //String username2;
+  Two(username) : super(username);
+
+  String get getVar {
+    return super.one;
+    // super always refers to the parent class ( in this case it is one )
+  }
+}
